e37623587e6c75aa4d7812b9f7d93ae414697ccb
Ouanis Seddaoui <ouanixi@gmail.com>
2015-08-21 20:53:25 +0100

    added comments

--- src/Grapher.hs
+++ src/Grapher.hs
@@ -9,10 +9,14 @@ import           Jparse
 type Atom = (Text, Text, Text)
 type Graphable = [(Atom, Atom,[Atom])]
 
+
+-- Handles the case of the Json Key being a Maybe [ASTId]
 extractAtoms :: Maybe [ASTId] -> [Atom]
 extractAtoms (Just (x:xs)) = (name x, modu x, package x) : extractAtoms (pure xs)
 extractAtoms _             = []
 
+
+-- Turns a Json object into a tuple that's acceptable by graphFromEdges
 extractGraphable :: ASTId -> (Atom , Atom, [Atom])
 extractGraphable ASTId {
                         name = name,
@@ -22,6 +26,9 @@ extractGraphable ASTId {
                         } = ((name,modu,package), (name,modu,package), extractAtoms dependencies)
 
 
+-- graphFromEdges :: Ord key => [(Atom, Atom, [Atom])]
+--                              -> (Graph, Vertex -> (Atom, Atom, [Atom]), Atom -> Maybe Vertex)
 dependencySort :: Graphable -> [Atom]
+dependencySort [] = []
 dependencySort x = let (g,v,_) = graphFromEdges x
             in Prelude.map (\x -> let (k,_,_) = v x in k) $ topSort g
