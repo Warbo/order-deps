ee19459831d6f5e6548775748e2fed6813228e8e
Ouanis Seddaoui <ouanixi@gmail.com>
2015-08-28 13:32:47 +0100

    arbitrary instances ok but don't generate empty lists

--- test-suite/AllTests.hs
+++ test-suite/AllTests.hs
@@ -1,7 +1,7 @@
 module AllTests (tests) where
 
 import           Data.Aeson
-import           Data.Text
+import           Data.Text hiding (length)
 import           Grapher
 import           Jparse
 import           Test.Tasty
@@ -20,6 +20,13 @@ import           Test.Tasty.QuickCheck as QC
 --                        return (head : tail)
 -- you can use that to generate a list of recursive calls
 
+-- data ASTId =
+--   ASTId {  name         :: Text
+--          , modu         :: Text
+--          , package      :: Text
+--          , dependencies :: Maybe [ASTId]
+--         } deriving (Show, Generic, Eq)
+
 geomList :: Gen a -> Gen [a]
 geomList g = oneof [return <$> g, (:) <$> g <*> geomList g]
 
@@ -34,12 +41,21 @@ instance Arbitrary ASTId where
                 ASTId <$> arbitrary <*> arbitrary <*> arbitrary <*> pure r
 
 
-
 tests :: TestTree
 tests = testGroup "Tests" [properties]
 
 properties :: TestTree
-properties = testGroup "Properties" [QC.testProperty "parsed" prop_parse_correct]
+properties = testGroup "Properties" [QC.testProperty "parsed" prop_parse_correct,
+                                     QC.testProperty "extracted" prop_extract_correct]
 
 prop_parse_correct :: ASTId -> Bool
 prop_parse_correct x =  (decode . encode $ x)  == Just x
+
+prop_extract_correct :: ASTId -> Bool
+prop_extract_correct a@(ASTId _ _ _ Nothing )  = let  (_, _ ,t) = extractGraphable a
+                                                 in t == []
+prop_extract_correct a@(ASTId _ _ _ (Just x) ) = let  (_, _ ,t) = extractGraphable a
+                                                in (length x) == length t
+
+
+
