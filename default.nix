bb4ff1fa4912be985c789ca6ec105d4842a09a42
Chris Warburton <chriswarbo@gmail.com>
2015-11-02 20:34:00 +0000

    Re-added nix expressions ;)

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, aeson, base, bytestring, containers, stdenv, tasty
+, tasty-quickcheck, text
+}:
+mkDerivation {
+  pname = "order-deps";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson base bytestring containers text
+  ];
+  testHaskellDepends = [
+    aeson base bytestring containers tasty tasty-quickcheck text
+  ];
+  homepage = "https://github.com/ouanixi/order-deps.git";
+  description = "Toplogical sort of AST's as per their dependencies";
+  license = stdenv.lib.licenses.gpl3;
+}
