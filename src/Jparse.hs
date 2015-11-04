15534b60d31d6a90244a2fd2def424a9c5cc0c88
Chris Warburton <chriswarbo@gmail.com>
2015-11-04 00:02:01 +0000

    Move stuff around

--- src/Jparse.hs
+++ src/Jparse.hs
@@ -2,8 +2,8 @@
 
 module Jparse where
 
-import           Data.Aeson
-import qualified Data.ByteString.Lazy as B
+
+
 import           Data.Graph
 import qualified Data.Text            as T
 import           Types
