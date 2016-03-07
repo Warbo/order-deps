4bb2fe6109036a6ab1a1da0a502e49797d751092
Chris Warburton <chriswarbo@gmail.com>
2016-03-07 22:17:40 +0000

    Split off executable

--- src/Grapher.hs
+++ src/Grapher.hs
@@ -2,6 +2,7 @@
 module Grapher where
 
 import           Data.Aeson
+import qualified Data.Array as A
 import qualified Data.ByteString.Lazy as B
 import           Data.Graph
 import           Data.Text  (Text)
@@ -40,9 +41,30 @@ parse s = case eitherDecode s of
                Right ps -> ps
 
 group :: [ASTId] -> [SCC Atom]
-group = stronglyConnComp . map extractGraphable
+group ids = stronglyConnComp atoms
+  where atoms = map extractGraphable ids
+
+group' :: [ASTId] -> [SCC (Atom, Atom, [Atom])]
+group' = stronglyConnCompR . map extractGraphable
+
+leaves :: [ASTId] -> [ASTId]
+leaves = filter (null . aDeps)
+
+-- Dependencies an SCC has on Atoms outside itself
+sccDeps :: SCC (Atom, Atom, [Atom]) -> [Atom]
+sccDeps s = case s of
+    AcyclicSCC a  -> atomDeps a
+    CyclicSCC  as -> let inAs x = any (\(y,_,_) -> x == y) as
+                      in filter (not . inAs) (concatMap atomDeps as)
+
+atomDeps (_, _, ds) = ds
 
 process :: [ASTId] -> B.ByteString
 process = renderAll . group
 
 atomToId (n, m, p) = ID { idPackage = p, idModule = m, idName = n }
+
+idToAtom i = (idName i, idModule i, idPackage i)
+
+atomsToAstId (a, _, ds) = ASTId { aId   = atomToId a,
+                                  aDeps = map atomToId ds }
--- tests/AllTests.hs
+++ tests/AllTests.hs
@@ -1,7 +1,8 @@
 module AllTests (tests) where
 
 import           Data.Aeson
-import           Data.Text hiding (length)
+import qualified Data.Graph as DG
+import qualified Data.Text as T
 import           Grapher
 import           HS2AST.Tests.Generators
 import           Test.Tasty
@@ -18,19 +19,12 @@ import Types
 --                        return (head : tail)
 -- you can use that to generate a list of recursive calls
 
--- data ASTId =
---   ASTId {  name         :: Text
---          , modu         :: Text
---          , package      :: Text
---          , dependencies :: Maybe [ASTId]
---         } deriving (Show, Generic, Eq)
-
 geomList :: Gen a -> Gen [a]
 geomList g = do x <- g
                 oneof [pure [], (x:) <$> geomList g]
 
-instance Arbitrary Text where
-  arbitrary = pack <$> arbitrary
+instance Arbitrary T.Text where
+  arbitrary = T.pack <$> arbitrary
 
 instance Arbitrary ASTId where
   arbitrary = do i <- arbitrary
@@ -41,8 +35,14 @@ instance Arbitrary ASTId where
                    }
 
 tests :: TestTree
-tests = testGroup "Tests" [QC.testProperty "parsed"    prop_parse,
-                           QC.testProperty "extracted" prop_extract]
+tests = testGroup "Tests" [
+  QC.testProperty "Decode and encode are inverse"   prop_parse,
+  QC.testProperty "Extracting graph preserves size" prop_extract,
+  QC.testProperty "Cyclic dependencies are grouped" prop_combinesCycles,
+  QC.testProperty "Independent IDs are grouped"     prop_combinesIndependent,
+  QC.testProperty "Leaves are taken from graph"     prop_leavesAreInGraph,
+  QC.testProperty "Leaves have no dependencies"     prop_leavesHaveNoDeps,
+  QC.testProperty "Can get deps of SCCs"            prop_sccDepsMatch]
 
 prop_parse :: ASTId -> Bool
 prop_parse x =  (decode . encode $ x)  == Just x
@@ -50,3 +50,42 @@ prop_parse x =  (decode . encode $ x)  == Just x
 prop_extract :: ASTId -> Bool
 prop_extract a = case extractGraphable a of
                       (_, _, t) -> length t == length (aDeps a)
+
+prop_combinesCycles i1 i2 = i1 /= i2 ==>
+  case Grapher.group [ASTId { aId = i1, aDeps = [i2] },
+                      ASTId { aId = i2, aDeps = [i1] }] of
+       [DG.CyclicSCC as] -> let as' = map atomToId as
+                             in as' `elem` [[i1, i2], [i2, i1]]
+       _                   -> False
+
+prop_combinesIndependent i1 i2 = i1 /= i2 ==>
+  case Grapher.group [ASTId { aId = i1, aDeps = [] },
+                      ASTId { aId = i2, aDeps = [] }] of
+       [DG.CyclicSCC as] -> let as' = map atomToId as
+                             in as' `elem` [[i1, i2], [i2, i1]]
+
+prop_leavesAreInGraph ids' = counterexample debug test
+  where test    = all (`elem` ids) leafIds
+        ids     = fillGraph ids'
+        leafIds = leaves ids
+        debug   = show (("ids", ids), ("leaves", leafIds))
+
+prop_leavesHaveNoDeps ids' = counterexample debug test
+  where test    = all (null . aDeps) leafIds
+        ids     = fillGraph ids'
+        leafIds = leaves ids
+        debug   = show (("ids", ids), ("leaves", leafIds))
+
+prop_sccDepsMatch ids = all (`elem` astDs) sccDs && all (`elem` sccDs) astDs
+  where sccDs = concatMap sccDeps (group' ids)
+        astDs = filter (not . inIds) (concatMap (map idToAtom . aDeps) ids)
+        inIds x = any ((== atomToId x) . aId) ids
+
+-- Helpers
+
+fillGraph :: [ASTId] -> [ASTId]
+fillGraph [] = []
+fillGraph (aid:aids) = aid:aids ++ map simple unknownDeps
+  where unknownDeps = filter (`notElem` ids) (aDeps aid)
+        ids         = map aId aids
+        simple x    = ASTId { aId = x, aDeps = [] }
